991baf6ca46e8b800458c5baafc064fb78c7d45d
camilocoelhogomes <camilo.coelho.gomes@gmail.com>
2023-08-25 23:39:28 -0300

    upodate the databases names

--- mongo/build.sh
+++ mongo/build.sh
@@ -1,3 +1,3 @@
-podman rmi -f mongo_gestao_contratos
-podman build -t mongo_gestao_contratos .
-podman run -p 27017:27017 -v gestao_contratos_data:/data/db --name mongo_gestao_contratos mongo_gestao_contratos
+podman rmi -f mongo
+podman build -t mongo .
+podman run -p 27017:27017 -v mongo_data:/data/db --name mongo mongo
--- mongo/buildEmpty.sh
+++ mongo/buildEmpty.sh
@@ -1,3 +1,3 @@
-podman volume rm -f gestao_contratos_data
-podman volume create gestao_contratos_data
+podman volume rm -f mongo_data
+podman volume create mongo_data
 bash build.sh
